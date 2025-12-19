3d46f91d41de820eb00aaaf6702b14642bbb7cc9
AlbeMiglio <dev@albemiglio.it>
2025-12-19 18:59:30 +0000

    added alias for num_epochs in argument parser. Enhance run name generation in logger with training parameters.

--- core/config.py
+++ core/config.py
@@ -21,7 +21,7 @@ class Config(object):
         self.split = "recordings"
         self.batch_size = 16
         self.test_batch_size = 1
-        self.num_epochs = 10
+        self.num_epochs = 100
         self.lr = 1e-3
         self.weight_decay = 1e-3
         self.log_interval = 5
@@ -56,6 +56,7 @@ class Config(object):
         parser.add_argument("--batch_size", type=int, default=16, help="batch size")
         parser.add_argument("--test-batch-size", type=int, default=1, help="input batch size for testing (default: 1000)")
         parser.add_argument("--num_epochs", type=int, default=10, help="number of epochs")
+        parser.add_argument("--epochs", type=int, dest="num_epochs", help="number of epochs (alias for --num_epochs)")
         parser.add_argument("--lr", type=float, default=1e-4, help="learning rate")
         parser.add_argument("--weight_decay", type=float, default=1e-3, help="weight decay")
         parser.add_argument("--ckpt", type=str, default=None, help="checkpoint path")
