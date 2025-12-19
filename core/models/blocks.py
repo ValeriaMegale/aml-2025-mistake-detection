b8efabd4f87e5cd99e43b866717ea4b282cbfc1e
LucaFavole <153202808+LucaFavole@users.noreply.github.com>
2025-12-19 18:11:20 +0100

    added dropout in RNN+ changed learning rate and pos_weight

--- core/models/blocks.py
+++ core/models/blocks.py
@@ -3,6 +3,8 @@ import math
 import torch
 import torch.nn as nn
 from torch import Tensor
+from torch.nn.functional import dropout
+
 from constants import Constants as const
 
 # define the transformer backbone here
@@ -41,18 +43,18 @@ import torch
 import torch.nn as nn
 
 class RNNBaseline(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, output_dim=1):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim=1, dropout=0.5):
         super(RNNBaseline, self).__init__()
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         
         # LSTM Layer
         # batch_first=True si aspetta input (Batch, Seq_Len, Features)
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True, dropout=dropout)
         
         # Classification Head
         self.fc = nn.Linear(hidden_dim, output_dim)
-
+        self.dropout_layer = nn.Dropout(dropout)
     def forward(self, x):
         # x shape potrebbe essere [batch_size, seq_len, input_dim] o [seq_len, input_dim]
         # Se manca la dimensione batch, aggiungila
